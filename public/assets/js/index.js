0589b04e7202dbca603702da258956ca8a039c7b
Fabitheguy003 <fabienmukungu@gmail.com>
2023-02-16 22:55:14 -0500

    fixed all error and noter taker working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -118,7 +118,9 @@ const handleRenderSaveBtn = () => {
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
-  let jsonNotes = await notes.json();
+  let jsonNotesString = await notes.json();
+  let jsonNotes =  JSON.parse (jsonNotesString);
+
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
